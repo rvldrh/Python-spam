03f96bccb40f7cb8cf7ad1ad35779825b18bab4b
rivaldo <112833805+rvldrh@users.noreply.github.com>
2023-02-09 20:12:23 +0700

    Add files via upload

---
+++ spamWA.py
@@ -0,0 +1,19 @@
+import pyautogui as spam
+import time
+import os
+limit = input("Enter no. of message : ")
+msg = input("yang mau di kirim : ")
+
+i = 0 
+pass
+pass
+pass
+property
+time.sleep(2)
+
+while i<int(limit):
+    spam.typewrite(msg)
+    spam.press('Enter')
+    i+=1
+os.system("exit")
+    
